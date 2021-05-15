f03ce115212e38f64fd746710e3a30f62c3b3c5c
SergioEanX <sergio.ferlito@enea.it>
2021-05-15 20:45:02 +0200

    refactored exporting class but not istance

--- dbMongo.js
+++ dbMongo.js
@@ -1,41 +1,5 @@
 const { MongoClient } = require("mongodb");
-const chalk = require("chalk");
-const info = chalk.keyword("white").bgGreen;
-
-// exports.client = async () => {
-//   /**
-//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-//    * See https://docs.mongodb.com/drivers/node/ for more details
-//    */
-//   const uri =
-//     "mongodb://monicadmin:Nitrox!100@127.0.0.1:27017?replicaSet=AirHeritage";
-
-//   /**
-//    * The Mongo Client you will use to interact with your database
-//    * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
-//    */
-//   const client = new MongoClient(uri, {
-//     useUnifiedTopology: true,
-//     connectTimeoutMS: 5000,
-//   });
-
-//   try {
-//     // Connect to the MongoDB cluster
-//     const conn = await client.connect();
-//     const { host, port } = conn.s.options.servers[0];
-//     console.log(
-//       info(
-//         `Successfully connected to mongodb host ${host} port ${port} replica set ${conn.s.options.replicaSet}`
-//       )
-//     );
-//     return client;
-//   } catch (err) {
-//     // Close the connection to the MongoDB cluster and exit App
-//     await client.close();
-//     console.log(er(`An error occured ${err}\nEXITING....`));
-//     process.exit(1);
-//   }
-// };
+const { chalk, info, er } = require("./utils/colored_console");
 class db {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
@@ -43,12 +7,13 @@ class db {
    */
   constructor() {
     const uri =
-      "mongodb://monicadmin:Nitrox!100@127.0.0.1:27017?replicaSet=AirHeritage";
+      "mongodb://monicadmin:Nitrox!100@127.0.0.1:27017?replicaSet=AirHeritage/Air-Heritage";
     /**
      * The Mongo Client you will use to interact with your database
      * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
      */
     this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       connectTimeoutMS: 5000,
     });
@@ -58,17 +23,24 @@ class db {
       // Connect to the MongoDB cluster
       const conn = await this.client.connect();
       const { host, port } = conn.s.options.servers[0];
+      const { replicaSet } = conn.s.options;
       console.log(
         info(
-          `Successfully connected to mongodb host ${host} port ${port} replica set ${conn.s.options.replicaSet}`
+          `Successfully connected to mongodb host ${host} port ${port} replica set ${replicaSet}`
         )
       );
     } catch (err) {
       // Close the connection to the MongoDB cluster and exit App
-      await this.client.close();
       console.log(er(`An error occured ${err}\nEXITING....`));
+      await this.client.close();
       process.exit(1);
     }
   }
+  // async close() {
+  //   if (this.client) {
+  //     await this.client.close();
+  //   }
+  //   console.log(info(`Connection to db closed at ${new Date()}`));
+  // }
 }
-module.exports = new db();
+module.exports = db;
