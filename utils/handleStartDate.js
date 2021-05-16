4b3e393d8960257fb3c19cc2425fd4f8bb78046b
SergioEanX <sergio.ferlito@enea.it>
2021-05-16 11:41:19 +0200

    load colored_console moduel

--- utils/handleStartDate.js
+++ utils/handleStartDate.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const path = require("path");
-const chalk = require("chalk");
-const info = chalk.keyword("white").bgGrey;
-const er = chalk.keyword("white").bgRedBright;
-const log = console.log;
+// load chalk colored console.log
+const { info } = require("./colored_console");
+
 // read and write file synchronously
+// save datetime last run in "lastRUN.json"
 exports.getOrSetStartDate = () => {
   const pathFile = path.join(__dirname, "../lastRUN.json");
   let startDate;
   if (fs.existsSync(pathFile)) {
     const data = fs.readFileSync(pathFile);
     startDate = new Date(JSON.parse(data).lastRun);
-    log(info(`LAST DB QUERY AT ${startDate}`));
+    console.log(info(`LAST DB QUERY AT ${startDate}`));
   } else {
     let data = JSON.stringify({ lastRun: new Date() });
     fs.writeFileSync(pathFile, data);
