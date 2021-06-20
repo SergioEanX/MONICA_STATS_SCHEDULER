da0eb38418b1d39e5592b3307629ae7dd9157312
SergioEanX <sergio.ferlito@enea.it>
2021-06-20 19:16:06 +0200

    added updateStartDate

--- utils/handleStartDate.js
+++ utils/handleStartDate.js
@@ -2,21 +2,23 @@ const fs = require("fs");
 const path = require("path");
 // load chalk colored console.log
 const { info } = require("./colored_console");
+const pathFile = path.join(__dirname, "../lastRUN.json");
 
 // read and write file synchronously
 // save datetime last run in "lastRUN.json"
-exports.getOrSetStartDate = () => {
-  const pathFile = path.join(__dirname, "../lastRUN.json");
+exports.getStartDate = () => {
   let startDate;
   if (fs.existsSync(pathFile)) {
     const data = fs.readFileSync(pathFile);
     startDate = new Date(JSON.parse(data).lastRun);
     console.log(info(`LAST DB QUERY AT ${startDate}`));
-  } else {
-    let data = JSON.stringify({ lastRun: new Date() });
-    fs.writeFileSync(pathFile, data);
   }
   // returns undefined if no JSON file was found otherwise returns the
   // startDate
   return startDate;
+};
+exports.updateStartDate = (t) => {
+  let data = JSON.stringify({ lastRun: t });
+  fs.writeFileSync(pathFile, data);
+  console.log(info(`LAST RUN updated to ${t}`));
 };
